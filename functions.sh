ac2d72b84611c043a514069c362466fa13e4a6db
Thomas de Lachaux <thomas.delachaux@gmail.com>
2020-06-09 16:02:04 +0200

    Sync working

---
+++ functions.sh
@@ -0,0 +1,112 @@
+# Log functions
+log() {
+  echo -e "\e[90m[$(date +"%Y-%m-%d %H:%M:%S")]\e[39m $@"
+}
+
+warn() {
+  log "\e[33mWARN:\e[39m $@"
+}
+
+info() {
+  log "\e[32mINFO:\e[39m $@"
+}
+
+debug() {
+  log "\e[34mDEBUG:\e[39m $@"
+}
+
+error() {
+  log "\e[31mERROR:\e[39m $@"
+  exit 1
+}
+
+# Folder functions
+checkFolders() {
+  [[ -d $folderA ]] || error "Le dossier A n'existe pas"
+  [[ -d $folderB ]] || error "Le dossier B n'existe pas"
+}
+
+# Liste le dossier récursivement et supprime le dossier racine
+listFolder() {
+  folderName=$1
+  find $folderName | cut -d / -f 2- | sed '1d'
+}
+
+# Journal functions
+listFolderExplicit() {
+  folderName=$1
+  # Supprime les lignes commençant par total
+  find $folderName -exec ls -ld --time-style='+%Y-%m-%d-%H-%M-%S' {} + | sed '1d' | sed "s/$folderName\///"
+}
+
+getFileMetadatas() {
+  ls -ld --time-style='+%Y-%m-%d-%H-%M-%S' $1 | awk '{print $1,$3,$4,$5,$6}'
+}
+
+getFileOwner() {
+  ls -ld $1 | awk '{print $3,$4}' | sed 's/\ /\:/'
+}
+
+# Help with https://bit.ly/3dqAJcN
+# Prints the permissions in octal
+getFilePermissions() {
+  ls -ld $1 | awk '{k=0;for(i=0;i<=8;i++)k+=((substr($1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf("%0o ",k);print $1}' | cut -c -3
+}
+
+
+getJournal() {
+  # Crée le journal s'il n'existe pas et synchronise A --> B
+  if [[ ! -f $journalPath ]]; then
+    rm -rf $folderB
+    cp -r $folderA $folderB
+    listFolderExplicit $folderA > $journalPath
+    echo "Dossier synchronisé"
+    exit 0
+  fi
+}
+
+getJournalFileName() {
+  file=$1
+  cat $journalPath | awk '{print $7}' | grep -Fx "$file"
+}
+
+# Retourne à quelle ligne du journal le fichier se situe
+getJournalFileLineLocation() {
+  file=$1
+  cat $journalPath | awk '{print $7}' | grep -Fnx "$file" | cut -d : -f 1
+}
+
+getJournalFileMetadatas() {
+  file=$1
+  line=$(getJournalFileLineLocation $file)
+  cat $journalPath | awk '{print $1,$3,$4,$5,$6}' | sed -n "${line}p"
+}
+
+# $1 source
+# $2 dest
+checkAndCopy() {
+  if [[ -f $1 ]]; then
+    cp -p $1 $2
+  else
+    # Verifie si le proprio/groupe a changé
+    if [[ $(getFileOwner $1) != $(getFileOwner $2) ]]; then
+      # Seul le root peut lancer chown, verification si on est root
+      if [[ $UID == 0 ]]; then
+        chown $(getFileOwner $1) $2
+      else
+        echo "La possession du dossier $1 est différente du dossier $2 et seul le root peut le modifier"
+      fi
+    fi
+
+    if [[ $(getFilePermissions $1) != $(getFilePermissions $2) ]]; then
+      chmod $(getFilePermissions $1) $2 2> /dev/null || echo "Vous n'avez pas les droits pour modifier $2"
+    fi
+  fi
+}
+
+# Path functions
+# Removes ./
+popCurrentDir() {
+  folder=$1
+  echo $folder | sed 's/^\.\///'
+}
--- main.sh
+++ main.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
+source ./functions.sh
 # Doit pouvoir être overridden par des variables d'environnement du même nom
 if [[ -d $1 ]]; then
 	folderA=$1
-else 
+else
 	echo "Le premier argument n'est pas un dossier."
 	exit 1
 fi
@@ -45,24 +46,12 @@ checkFolders() {
   [[ -d $folderB ]] || error "Le dossier B n'existe pas"
 }
 
-# Liste le dossier récursivement et supprime le dossier racine
-listFolder() {
-  folderName=$1
-  find $folderName | cut -d / -f 2- | sed '1d'
-}
-
-listFolderExplicit() {
-  folderName=$1
-  # Supprime les lignes commençant par total
-  find $folderName -exec ls -ld --time-style='+%Y-%m-%d %H:%m' {} + | sed '1d' | sed "s/$folderName\///"
-}
-
 
 getJournal() {
   # Crée le journal s'il n'existe pas et synchronise A --> B
   if [[ ! -f $journalPath ]]; then
     rm -rf $folderB
-    cp -r $folderA $folderB
+    cp -pr $folderA $folderB
     listFolderExplicit $folderA > $journalPath
     echo "Dossier synchronisé"
     exit 0
@@ -70,30 +59,99 @@ getJournal() {
 }
 
 listJournal() {
-  cat $journalPath | awk '{print $8}'
+  cat $journalPath | awk '{print $7}'
 }
 
 checkFolders
 getJournal
 
+sync() {
 
 for file in $(listFolder $folderA); do
+
+  folderA=$1
+  folderB=$2
+
   metadataA=$(ls $folderA/$file)
 
   # Check if file or directory exists
-  if [[ ! -e $folderB/$file ]]; then
+  if [[ -e $folderB/$file ]]; then
+
+    if [[ -f $folderA/$file && -d $folderB/$file ]]; then
+      echo "Conflit ! $folderA/$file est un fichier et $folderB/$file est un dossier"
+
+    elif [[ -d $folderA/$file && -f $folderB/$file ]]; then
+      echo "Conflit ! $folderA/$file est un dossier et $folderB/$file est un fichier"
+    
+    # Récupère le fichier dans le journal
+    elif [[ $(getJournalFileName ${file}) ]]; then
+      journalDate=$(getJournalFileMetadatas ${file})
+
+      if [[ $(getFileMetadatas $folderA/$file) != $(getFileMetadatas $folderB/$file) ]]; then
+        if [[ $journalDate != $(getFileMetadatas $folderA/$file) && $journalDate != $(getFileMetadatas $folderB/$file) ]]; then
+
+          unset REPLY
+
+          while [[ $REPLY != 1 || $REPLY != 2 ]] ; do
+            echo "Conflit sur le fichier $file !"
+            echo "1) Garder $folderA/$file"
+            echo "2) Garder $folderB/$file"
+
+            read
+      
+            if [[ $REPLY == 1 ]]; then
+              checkAndCopy $folderA/$file $folderB/$file
+              break
+            elif [[ $REPLY == 2 ]]; then
+              checkAndCopy $folderB/$file $folderA/$file
+              break
+            else 
+              echo "Entrée incorrecte"
+            fi
+          done
+
+        else
+          if [[ $journalDate !=  $(getFileMetadatas $folderA/$file) ]]; then
+            echo "[Dossier A] $file modifié"
+            checkAndCopy $folderA/$file $folderB/$file
+            
+          elif [[ $journalDate !=  $(getFileMetadatas $folderB/$file) ]]; then
+            echo "[Dossier B] $file modifié"
+
+            checkAndCopy $folderB/$file $folderA/$file
+          fi
+        fi
+      else
+        echo "fichier non modifié"
+      fi
+
+      echo "journal: $journalDate"
+      getFileMetadatas $folderA/$file
+      getFileMetadatas $folderB/$file
+
+      #echo lol
+    else
+      # todo: gerer cette erreur
+      error "Une erreur est survenue"
+      exit 1
+    fi
+
+  else
     echo "Fichier manquant ! : $file"
     # grep -Fx checks all the line and ignore special character which can be in the ${file}
-    if [[ $(listJournal | grep -Fx "${file}") ]]; then
+    if [[ $(getJournalFileName ${file}) ]]; then
       # Le fichier existe dans le journal
       rm -rv $folderA/$file
 
     else
       echo "Le fichier n'existe pas dans le journal"
       # check si le fichier est un dossier ou ono
-      [[ -d $folderA/$file ]] && cp -vr --parents $folderA/$file $folderB/$file || cp $folderA/$file $folderB/$file
+      [[ -d $folderA/$file ]] && cp -pr $folderA/$file $folderB || cp -p $folderA/$file $folderB/$file
 
     fi
   fi
 done
 listFolderExplicit $folderA > $journalPath
+}
+
+sync $folderA $folderB
